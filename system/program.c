2684b3b15ff1010a34389b67e27376c58ab87655
Adrian Herridge <adrian@uncia.co.uk>
2026-09-14 22:03:29 +0100

    Stop high-frequency display diagnostics hitting the SD card

--- system/program.c
+++ system/program.c
@@ -913,7 +913,6 @@ void program_scheduler_step(void) {
         program_terminate(p);
         return;
     }
-    program_log_event(p, "tick-return", NULL);
     if (p != s_top) {
         return; /* tick launched a program or replaced this one */
     }
